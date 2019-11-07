7296e76a50b17a4077e3b248db85e82e6ea3657c
mr-artur <ar2r.pv@gmail.com>
2019-11-07 16:25:27 +0200

    almost finished facebook connect resolver

---
+++ taxi-server/src/api/User/FacebookConnect/FacebookConnect.resolvers.ts
@@ -0,0 +1,50 @@
+import User from "../../../entities/User";
+import {
+  FacebookConnectMutationArgs,
+  FacebookConnectResponse
+} from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    FacebookConnect: async (
+      _,
+      args: FacebookConnectMutationArgs
+    ): Promise<FacebookConnectResponse> => {
+      const { fbId } = args;
+      try {
+        const existingUser = await User.findOne({ fbId });
+        if (existingUser) {
+          return {
+            ok: true,
+            error: null,
+            token: "Will add soon"
+          };
+        }
+      } catch (e) {
+        return {
+          ok: false,
+          error: e.message,
+          token: null
+        };
+      }
+      try {
+        await User.create({
+          ...args,
+          profilePhoto: `http://graph.facebook.com/${fbId}/picture?type=square`
+        }).save();
+        return {
+          ok: true,
+          error: null,
+          token: "Will add soon"
+        };
+      } catch (e) {
+        return {
+          ok: false,
+          error: e.message,
+          token: null
+        };
+      }
+    }
+  }
+};
