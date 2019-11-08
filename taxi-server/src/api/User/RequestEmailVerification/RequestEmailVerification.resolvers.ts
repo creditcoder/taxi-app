b2eaeb9da1221ef379c6f7db3f28cad19d3fa593
mr-artur <ar2r.pv@gmail.com>
2019-11-08 23:44:01 +0200

    requestEmailVerification resolver

---
+++ taxi-server/src/api/User/RequestEmailVerification/RequestEmailVerification.resolvers.ts
@@ -0,0 +1,47 @@
+import User from "../../../entities/User";
+import Verification from "../../../entities/Verification";
+import { RequestEmailVerificationResponse } from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
+import checkAuthResolver from "../../../utils/authResolver";
+import { sendVerificationEmail } from "../../../utils/sendEmail";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    RequestEmailVerification: checkAuthResolver(
+      async (_, __, { req }): Promise<RequestEmailVerificationResponse> => {
+        const user: User = req.user;
+        if (user.email) {
+          try {
+            const oldVerification = await Verification.findOne({
+              payload: user.email
+            });
+            if (oldVerification) {
+              oldVerification.remove();
+            }
+            const newVerification = await Verification.create({
+              payload: user.email,
+              target: "EMAIL"
+            }).save();
+            await sendVerificationEmail(user.fullName, newVerification.key);
+            return {
+              ok: true,
+              error: null
+            };
+          } catch (e) {
+            return {
+              ok: false,
+              error: e.message
+            };
+          }
+        } else {
+          return {
+            ok: false,
+            error: "That user has no email"
+          };
+        }
+      }
+    )
+  }
+};
+
+export default resolvers;
