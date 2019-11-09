7b4df5f823563b4375f0a56d8eb07618cc21e71c
mr-artur <ar2r.pv@gmail.com>
2019-11-09 16:51:41 +0200

    edit place resolver

---
+++ taxi-server/src/api/Place/EditPlace/EditPlace.resolvers.ts
@@ -0,0 +1,50 @@
+import Place from "../../../entities/Place";
+import User from "../../../entities/User";
+import { EditPlaceMutationArgs, EditPlaceResponse } from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
+import checkAuthResolver from "../../../utils/authResolver";
+import clearNullArgs from "../../../utils/clearNullArgs";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    EditPlace: checkAuthResolver(
+      async (
+        _,
+        args: EditPlaceMutationArgs,
+        { req }
+      ): Promise<EditPlaceResponse> => {
+        const user: User = req.user;
+        try {
+          const place = await Place.findOne({ id: args.placeId });
+          if (place) {
+            if (place.userId === user.id) {
+              const notNullArgs = clearNullArgs(args);
+              await Place.update({ id: args.placeId }, { ...notNullArgs });
+              return {
+                ok: true,
+                error: null
+              };
+            } else {
+              return {
+                ok: false,
+                error: "Not authorized"
+              };
+            }
+          } else {
+            return {
+              ok: false,
+              error: "Place was not found"
+            };
+          }
+        } catch (e) {
+          return {
+            ok: false,
+            error: e.message
+          };
+        }
+      }
+    )
+  }
+};
+
+export default resolvers;
