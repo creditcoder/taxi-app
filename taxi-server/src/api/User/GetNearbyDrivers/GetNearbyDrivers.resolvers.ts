af1883aeb9ccfd4c197822cf7b28e1ae38bca25e
mr-artur <ar2r.pv@gmail.com>
2019-11-09 19:16:53 +0200

    finished get nearby drivers resolver

--- taxi-server/src/api/User/GetNearbyDrivers/GetNearbyDrivers.resolvers.ts
+++ taxi-server/src/api/User/GetNearbyDrivers/GetNearbyDrivers.resolvers.ts
@@ -1,4 +1,4 @@
-import { Between } from "typeorm";
+import { Between, getRepository } from "typeorm";
 import User from "../../../entities/User";
 import { GetNearbyDriversResponse } from "../../../types/graph";
 import { Resolvers } from "../../../types/resolvers";
@@ -10,11 +10,25 @@ const resolvers: Resolvers = {
       async (_, __, { req }): Promise<GetNearbyDriversResponse> => {
         const user: User = req.user;
         const { lastLat, lastLng } = user;
-        const drivers = await User.find({
-          isDriving: true,
-          // lat between
-          // lng between
-        })
+        try {
+          // find operators work only with repositories
+          const drivers: User[] = await getRepository(User).find({
+            isDriving: true,
+            lastLat: Between(lastLat - 0.05, lastLat + 0.05),
+            lastLng: Between(lastLng - 0.05, lastLng + 0.05)
+          });
+          return {
+            ok: true,
+            error: null,
+            drivers
+          };
+        } catch (e) {
+          return {
+            ok: false,
+            error: e.message,
+            drivers: null
+          };
+        }
       }
     )
   }
