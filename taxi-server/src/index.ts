be70facb3af661bf16f022fbba55efa73504f937
mr-artur <ar2r.pv@gmail.com>
2019-11-10 15:14:28 +0200

    started authenticating ws subscriptions

--- taxi-server/src/index.ts
+++ taxi-server/src/index.ts
@@ -4,15 +4,34 @@ import { Options } from "graphql-yoga";
 import { createConnection } from "typeorm";
 import app from "./app";
 import connectionOptions from "./ormConfig";
+import decodeJWT from "./utils/decodeJWT";
 
 const PORT: number | string = process.env.PORT || 4000;
 const PLAYGROUND_ENDPOINT: string = "/playground";
 const GRAPHQL_ENDPOINT: string = "/graphql";
+const SUBSCRIPTION_ENDPOINT: string = "/subscription";
 
 const appOptions: Options = {
   port: PORT,
   playground: PLAYGROUND_ENDPOINT,
-  endpoint: GRAPHQL_ENDPOINT
+  endpoint: GRAPHQL_ENDPOINT,
+  subscriptions: {
+    path: SUBSCRIPTION_ENDPOINT,
+    onConnect: async connectionParams => {
+      const token = connectionParams["X-JWT"];
+      if (token) {
+        const user = await decodeJWT(token);
+        if (user) {
+          return {
+            // when we return current user, it will be added to the
+            // context of subscription resolver
+            currentUser: user
+          };
+        }
+      }
+      throw new Error("No token found. Can not subscribe");
+    }
+  }
 };
 
 const handleAppStart = () => console.log(`Listening on port ${PORT}`);
