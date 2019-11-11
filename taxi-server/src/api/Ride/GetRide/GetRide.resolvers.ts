b0b08e12ded7ee55fa15f8bc940c04075d9ba4f0
mr-artur <ar2r.pv@gmail.com>
2019-11-11 13:14:54 +0200

    get ride resolver

---
+++ taxi-server/src/api/Ride/GetRide/GetRide.resolvers.ts
@@ -0,0 +1,50 @@
+import Ride from "../../../entities/Ride";
+import User from "../../../entities/User";
+import { GetRideQueryArgs, GetRideResponse } from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
+import checkAuthResolver from "../../../utils/authResolver";
+
+const resolvers: Resolvers = {
+  Query: {
+    GetRide: checkAuthResolver(
+      async (_, args: GetRideQueryArgs, { req }): Promise<GetRideResponse> => {
+        const user: User = req.user;
+        try {
+          const ride = await Ride.findOne(
+            {
+              id: args.rideId
+            }
+            // ,{ relations: ["passenger", "driver"] }
+          );
+          if (ride) {
+            if (ride.passengerId === user.id || ride.driverId === user.id) {
+              return {
+                ok: true,
+                error: null,
+                ride
+              };
+            } else {
+              return {
+                ok: false,
+                error: "Not authorized",
+                ride: null
+              };
+            }
+          } else {
+            return {
+              ok: false,
+              error: "Ride was not found",
+              ride: null
+            };
+          }
+        } catch (e) {
+          return {
+            ok: false,
+            error: e.message,
+            ride: null
+          };
+        }
+      }
+    )
+  }
+};
