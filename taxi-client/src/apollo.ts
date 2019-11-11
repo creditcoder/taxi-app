e4ce1aaa3cf7ec0f8a0709251419208743ae33fb
mr-artur <ar2r.pv@gmail.com>
2019-11-11 21:59:09 +0200

    setting jwt header with token from local storage

--- taxi-client/src/apollo.ts
+++ taxi-client/src/apollo.ts
@@ -1,6 +1,14 @@
-import ApolloClient from "apollo-boost";
+import ApolloClient, { Operation } from "apollo-boost";
+import { async } from "q";
 
 const client = new ApolloClient({
+  request: async (operation: Operation) => {
+    operation.setContext({
+      headers: {
+        "X-JWT": localStorage.getItem("jwt") || ""
+      }
+    });
+  },
   uri: "http://localhost:4000/graphql"
 });
 
