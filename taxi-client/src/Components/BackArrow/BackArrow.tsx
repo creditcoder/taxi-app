32511d8a3ad9080cda0186669b33eb76d47181cc
mr-artur <ar2r.pv@gmail.com>
2019-11-12 23:31:54 +0200

    finished back arrow component

--- taxi-client/src/Components/BackArrow/BackArrow.tsx
+++ taxi-client/src/Components/BackArrow/BackArrow.tsx
@@ -2,15 +2,28 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "../../typed-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  transform: scale(0.8);
+`;
 
 interface IProps {
   backTo: string;
+  className?: string;
 }
 
-const BackArrow: React.FC<IProps> = ({ backTo }) => (
-  <Container>
-    <Link to={backTo}>here will be svg</Link>
+const BackArrow: React.FC<IProps> = ({ backTo, className }) => (
+  <Container className={className}>
+    <Link to={backTo}>
+      <svg
+        width="24"
+        height="24"
+        xmlns="http://www.w3.org/2000/svg"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+      >
+        <path d="M20 .755l-14.374 11.245 14.374 11.219-.619.781-15.381-12 15.391-12 .609.755z" />
+      </svg>
+    </Link>
   </Container>
 );
 
