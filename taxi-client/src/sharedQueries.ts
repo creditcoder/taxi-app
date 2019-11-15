0dc6142cb5eabfaac50d81e8fd9b5e2bb0dda251
mr-artur <ar2r.pv@gmail.com>
2019-11-15 18:11:35 +0200

    fixed an issue: now saving jwt in cache works

--- taxi-client/src/sharedQueries.ts
+++ taxi-client/src/sharedQueries.ts
@@ -2,6 +2,6 @@ import { gql } from "apollo-boost";
 
 export const LOG_USER_IN = gql`
   mutation logUserIn($token: String!) {
-    logUserIn(token: $token) @clients
+    logUserIn(token: $token) @client
   }
 `;
