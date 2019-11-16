063d3688fc726b181a95e4ad3d099fb1ecaeed3a
mr-artur <ar2r.pv@gmail.com>
2019-11-16 14:44:33 +0200

    created home component

---
+++ taxi-client/src/Routes/Home/HomePresenter.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Helmet from "react-helmet";
+import Layout from "../../Components/Container";
+
+const HomePresenter: React.FC = () => (
+  <Layout>
+    <Helmet>
+      <title>Home | Taxi</title>
+    </Helmet>
+  </Layout>
+);
+
+export default HomePresenter;
--- taxi-client/src/Routes/Home/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Home = () => <div>Home Component</div>;
-
-export default Home;
