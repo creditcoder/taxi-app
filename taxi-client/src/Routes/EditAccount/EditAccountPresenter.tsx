856d4ca14ab4a2965fcff7b98e71b97c41a3cf3d
mr-artur <ar2r.pv@gmail.com>
2019-11-16 23:36:02 +0200

    edit account form and styles

--- taxi-client/src/Routes/EditAccount/EditAccountPresenter.tsx
+++ taxi-client/src/Routes/EditAccount/EditAccountPresenter.tsx
@@ -1,6 +1,32 @@
 import React from "react";
 import Helmet from "react-helmet";
+import Button from "../../Components/Button";
 import Layout from "../../Components/Container";
+import Form from "../../Components/Form";
+import Header from "../../Components/Header";
+import Input from "../../Components/Input";
+import styled from "../../typed-components";
+
+const ExtendedForm = styled(Form)`
+  height: 250px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 40px;
+  margin-bottom: 30px;
+`;
+
+const ExtendedButton = styled(Button)`
+  border-radius: 10px;
+  background-color: ${props => props.theme.blueColor};
+  box-shadow: 0 2px 25px ${props => props.theme.blueColor};
+  transition: 0.3s;
+  &:hover {
+    background-color: ${props => props.theme.violetColor};
+  box-shadow: 0 2px 25px ${props => props.theme.violetColor};
+  }
+`;
 
 interface IProps {
   firstName: string;
@@ -21,6 +47,34 @@ const EditAccountPresenter: React.FC<IProps> = ({
     <Helmet>
       <title>Edit Account | Taxi</title>
     </Helmet>
+    <Header title={"Edit Account"} backTo={"/"} />
+    <ExtendedForm>
+      <Input
+        width={"250px"}
+        onChange={onInputChange}
+        type={"text"}
+        value={firstName}
+        placeholder={"First name"}
+        name="firstName"
+      />
+      <Input
+        width={"250px"}
+        onChange={onInputChange}
+        type={"text"}
+        value={lastName}
+        placeholder={"Last name"}
+        name="lastName"
+      />
+      <Input
+        width={"250px"}
+        onChange={onInputChange}
+        type={"email"}
+        value={email}
+        placeholder={"Email"}
+        name="email"
+      />
+      <ExtendedButton onClick={null} value={"Update"} />
+    </ExtendedForm>
   </Layout>
 );
 
