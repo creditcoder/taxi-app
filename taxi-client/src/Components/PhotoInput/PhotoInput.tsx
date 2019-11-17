5a05419a12e2012347f2db6757f7d0f070a07c6d
mr-artur <ar2r.pv@gmail.com>
2019-11-17 18:06:36 +0200

    photo input component

---
+++ taxi-client/src/Components/PhotoInput/PhotoInput.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { rotate } from "../../animations";
+import styled from "../../typed-components";
+
+const Container = styled.div``;
+
+const Input = styled.input`
+  color: white;
+  opacity: 0;
+  height: 1px;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Image = styled.label`
+  height: 80px;
+  width: 80px;
+  display: flex;
+  align-items: center;
+  margin-bottom: 35px;
+  overflow: hidden;
+  cursor: pointer;
+  border: 2px solid ${props => props.theme.violetColor};
+  border-radius: 50%;
+  font-size: 28px;
+  & img {
+    height: 80px;
+    width: 80px;
+  }
+`;
+
+const Uploading = styled.span`
+  animation: ${rotate} 2s linear infinite;
+`;
+
+interface IProps {
+  uploading: boolean;
+  fileUrl: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const PhotoInput: React.FC<IProps> = ({ uploading, fileUrl, onChange }) => (
+  <Container>
+    <Input id={"photo"} type={"file"} accept="image/*" onChange={onChange} />
+    <Image htmlFor="photo">
+      {uploading ? (
+        <Uploading>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="48"
+            height="48"
+            viewBox="0 0 24 24"
+            fill="#5b0c75"
+          >
+            <path d="M14 22c0 1.104-.896 2-2 2s-2-.896-2-2 .896-2 2-2 2 .896 2 2zm-2-22c-1.104 0-2 .896-2 2s.896 2 2 2 2-.896 2-2-.896-2-2-2zm10 10c1.104 0 2 .896 2 2s-.896 2-2.001 2c-1.103 0-1.999-.895-1.999-2s.896-2 2-2zm-22 2c0 1.105.896 2 2 2s2-.895 2-2c0-1.104-.896-2-2-2s-2 .896-2 2zm19-9c1.104 0 2 .896 2 2s-.896 2-2.001 2c-1.103 0-1.999-.895-1.999-2s.896-2 2-2zm0 14c1.104 0 2 .896 2 2s-.896 2-2.001 2c-1.103 0-1.999-.895-1.999-2s.896-2 2-2zm-14-14c1.104 0 2 .896 2 2s-.896 2-2.001 2c-1.103 0-1.999-.895-1.999-2s.896-2 2-2zm0 14c1.104 0 2 .896 2 2s-.896 2-2.001 2c-1.103 0-1.999-.895-1.999-2s.896-2 2-2z" />
+          </svg>
+        </Uploading>
+      ) : (
+        <img src={fileUrl} />
+      )}
+    </Image>
+  </Container>
+);
+
+export default PhotoInput;
