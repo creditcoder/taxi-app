a03527cde2ce0168147d881ed1d2f96ddca66513
mr-artur <ar2r.pv@gmail.com>
2019-11-20 18:07:39 +0200

    displaying current position of user on the map

--- taxi-client/src/Routes/FindAddress/FindAddressContainer.tsx
+++ taxi-client/src/Routes/FindAddress/FindAddressContainer.tsx
@@ -1,31 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { toast } from "react-toastify";
 import FindAddressPresenter from "./FindAddressPresenter";
 
 class FindAddressContainer extends React.Component<any> {
   public mapRef: any;
   public map: google.maps.Map;
+
   constructor(props) {
     super(props);
     this.mapRef = React.createRef();
   }
+
   public componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      this.handleGeoSuccess,
+      this.handleGeoError
+    );
+  }
+
+  public handleGeoSuccess = (position: Position) => {
+    const {
+      coords: { latitude, longitude }
+    } = position;
+    this.loadMap(latitude, longitude);
+  };
+
+  public handleGeoError = () => {
+    toast.error("Error in getting your current position");
+  };
+
+  public render() {
+    return <FindAddressPresenter mapRef={this.mapRef} />;
+  }
+
+  public loadMap = (lat, lng) => {
     const { google } = this.props;
     const maps = google.maps;
     const mapNode = ReactDOM.findDOMNode(this.mapRef.current);
     const mapConfig: google.maps.MapOptions = {
       center: {
-        lat: 50.27,
-        lng: 30.3
+        lat,
+        lng
       },
+      disableDefaultUI: true,
       zoom: 11
     };
     this.map = new maps.Map(mapNode, mapConfig);
-  }
-
-  public render() {
-    return <FindAddressPresenter mapRef={this.mapRef} />;
-  }
+  };
 }
 
 export default FindAddressContainer;
--- taxi-client/src/Routes/FindAddress/FindAddressPresenter.tsx
+++ taxi-client/src/Routes/FindAddress/FindAddressPresenter.tsx
@@ -8,7 +8,22 @@ const Map = styled.div`
   left: 0;
   height: 100%;
   width: 100%;
+  z-index: 1;
 `;
+
+const Centered = styled.div`
+  position: absolute;
+  width: 30px;
+  height: 30px;
+  z-index: 2;
+  margin: auto;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`;
+
+const Icon = styled.span``;
 
 interface IProps {
   mapRef: any;
@@ -22,6 +37,18 @@ class FindAddressPresenter extends React.Component<IProps> {
         <Helmet>
           <title>Find Address | Taxi</title>
         </Helmet>
+        <Centered>
+          <Icon>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z" />
+            </svg>
+          </Icon>
+        </Centered>
         <Map ref={mapRef} />
       </div>
     );
