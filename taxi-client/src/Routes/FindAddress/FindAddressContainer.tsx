21fb12381219db993beab3b200583ff0991d4f10
mr-artur <ar2r.pv@gmail.com>
2019-11-20 17:36:59 +0200

    displaying map in find addr component

--- taxi-client/src/Routes/FindAddress/FindAddressContainer.tsx
+++ taxi-client/src/Routes/FindAddress/FindAddressContainer.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import FindAddressPresenter from "./FindAddressPresenter";
 
-class FindAddressContainer extends React.Component {
+class FindAddressContainer extends React.Component<any> {
+  public mapRef: any;
+  public map: google.maps.Map;
+  constructor(props) {
+    super(props);
+    this.mapRef = React.createRef();
+  }
+  public componentDidMount() {
+    const { google } = this.props;
+    const maps = google.maps;
+    const mapNode = ReactDOM.findDOMNode(this.mapRef.current);
+    const mapConfig: google.maps.MapOptions = {
+      center: {
+        lat: 50.27,
+        lng: 30.3
+      },
+      zoom: 11
+    };
+    this.map = new maps.Map(mapNode, mapConfig);
+  }
+
   public render() {
-    return <FindAddressPresenter />;
+    return <FindAddressPresenter mapRef={this.mapRef} />;
   }
 }
 
