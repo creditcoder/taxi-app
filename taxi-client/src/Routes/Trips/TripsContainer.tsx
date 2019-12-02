7be22e1abde994f7f0529d8c28712f827dae5e60
mr-artur <ar2r.pv@gmail.com>
2019-12-02 22:42:52 +0200

    getting trips data from api to trips screen

--- taxi-client/src/Routes/Trips/TripsContainer.tsx
+++ taxi-client/src/Routes/Trips/TripsContainer.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Query } from "react-apollo";
+import { getMyRides } from "../../types/api";
 import TripsPresenter from "./TripsPresenter";
+import { GET_MY_RIDES } from "./TripsQueries";
+
+class GetMyRidesQuery extends Query<getMyRides> {}
 
 class TripsContainer extends React.Component {
   public render() {
-    return <TripsPresenter />;
+    return (
+      <GetMyRidesQuery query={GET_MY_RIDES}>
+        {({ data, loading }) => (
+          <TripsPresenter data={data} loading={loading} />
+        )}
+      </GetMyRidesQuery>
+    );
   }
 }
 
--- taxi-client/src/Routes/Trips/TripsPresenter.tsx
+++ taxi-client/src/Routes/Trips/TripsPresenter.tsx
@@ -2,13 +2,50 @@ import React from "react";
 import Helmet from "react-helmet";
 import Layout from "../../Components/Container";
 import Header from "../../Components/Header";
+import { getMyRides } from "../../types/api";
 
-const TripsPresenter: React.FC = () => (
+interface IProps {
+  data?: getMyRides;
+  loading: boolean;
+}
+
+const TripsPresenter: React.FC<IProps> = ({
+  data: {
+    GetMyRides: { ridesAsDriver = null, ridesAsPassenger = null } = {}
+  } = {},
+  loading
+}) => (
   <Layout>
     <Helmet>
       <title>My Trips | Taxi</title>
     </Helmet>
     <Header backTo={"/"} title={"My Trips"} />
+    {!loading &&
+      ridesAsPassenger &&
+      ridesAsPassenger.length > 0 &&
+      ridesAsPassenger.map(ride => (
+        <div key={ride!.id}>
+          <div>{ride!.status}</div>
+          <div>{ride!.pickUpAddress}</div>
+          <div>{ride!.dropOffAddress}</div>
+          <div>{ride!.price}</div>
+          <div>{ride!.distance}</div>
+          <div>{ride!.driver!.fullName}</div>
+        </div>
+      ))}
+      {!loading &&
+      ridesAsDriver &&
+      ridesAsDriver.length > 0 &&
+      ridesAsDriver.map(ride => (
+        <div key={ride!.id}>
+          <div>{ride!.status}</div>
+          <div>{ride!.pickUpAddress}</div>
+          <div>{ride!.dropOffAddress}</div>
+          <div>{ride!.price}</div>
+          <div>{ride!.distance}</div>
+          <div>{ride!.passenger!.fullName}</div>
+        </div>
+      ))}
   </Layout>
 );
 
